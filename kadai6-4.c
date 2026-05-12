5e7d481622898f23d1e1490d378c36df119760a0
Wataru Yasue <cod14133@ict.nitech.ac.jp>
2026-05-12 13:51:45 +0900

    change variable name

--- kadai6-4.c
+++ kadai6-4.c
@@ -18,7 +18,7 @@ int str_equalnum(const char *s1, const char *s2)
 int main(void)
 {
 	char str1[16], str2[16];
-	int n;
+	int number;
 	
 	printf("文字列1を入力してください：");
 	scanf("%s", str1);
@@ -26,9 +26,9 @@ int main(void)
 	printf("文字列2を入力してください：");
 	scanf("%s", str2);
 	
-	n = str_equalnum(str1, str2);
+	number = str_equalnum(str1, str2);
 
-	printf("文字列1と文字列2は%d文字一致しています\n", n);
+	printf("文字列1と文字列2は%d文字一致しています\n", number);
 
 	return(0);
 }
